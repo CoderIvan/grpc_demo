aa3e5cb4d282523e1f33a3ec1f0445f7e2ae1730
XueHua Huang <xuehua.huang@dbjtech.com>
2021-04-15 11:47:10 +0800

    First init

---
+++ projects/demo01/client/index.js
@@ -0,0 +1,25 @@
+const PROTO_PATH = `${__dirname}/../common/helloworld.proto`
+const grpc = require('@grpc/grpc-js')
+const protoLoader = require('@grpc/proto-loader')
+const { port } = require('../common/config')
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+	keepCase: true,
+	longs: String,
+	enums: String,
+	defaults: true,
+	oneofs: true,
+})
+const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld
+
+const client = new hello_proto.Greeter(`localhost:${port}`, grpc.credentials.createInsecure())
+
+function main() {
+	const request = { name: 'world' }
+	console.log('[Client] >> %j', request)
+	client.sayHello(request, (err, response) => {
+		console.log('[Client] << %j', response)
+	})
+}
+
+main()
---
+++ projects/demo01/server/index.js
@@ -0,0 +1,36 @@
+const PROTO_PATH = `${__dirname}/../common/helloworld.proto`
+const grpc = require('@grpc/grpc-js')
+const protoLoader = require('@grpc/proto-loader')
+const { port } = require('../common/config')
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+	keepCase: true,
+	longs: String,
+	enums: String,
+	defaults: true,
+	oneofs: true,
+})
+const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld
+
+function sayHello(request) {
+	return { message: `Hello ${request.name}` }
+}
+
+function sayHelloHandle(call, callback) {
+	console.log('[Server] << %j', call.request)
+	const response = sayHello(call.request)
+	callback(null, response)
+	console.log('[Server] >> %j', response)
+}
+
+function main() {
+	const server = new grpc.Server()
+	server.addService(hello_proto.Greeter.service, { sayHello: sayHelloHandle })
+
+	server.bindAsync(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure(), () => {
+		server.start()
+		console.log('[Server] Listen @ %d', port)
+	})
+}
+
+main()
