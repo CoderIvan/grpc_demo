aed0fed4652f26cc26c7d8acf7502bf0fee15dbd
XueHua Huang <xuehua.huang@dbjtech.com>
2021-04-20 13:58:18 +0800

    temp

--- projects/demo02/client/index.js
+++ projects/demo02/client/index.js
@@ -7,6 +7,7 @@ const grpc = require('@grpc/grpc-js')
 const protoLoader = require('@grpc/proto-loader')
 
 const { port } = require('../common/config')
+const feature_list = require('../common/route_guide_db')
 
 const packageDefinition = protoLoader.loadSync(path.join(__dirname, '../common/route_guide.proto'), {
 	keepCase: true,
@@ -23,9 +24,8 @@ const COORD_FACTOR = 1e7
 
 async function send(point) {
 	const feature = await client.getFeatureAsync(point)
-
-	if (feature.name === '') {
-		console.log(`Found no feature at ${feature.location.latitude / COORD_FACTOR}, ${feature.location.longitude / COORD_FACTOR}`)
+	if (!feature.name) {
+		console.log(`Found no feature at ${point.latitude / COORD_FACTOR}, ${point.longitude / COORD_FACTOR}`)
 	} else {
 		console.log(`Found feature called "${feature.name}" at ${feature.location.latitude / COORD_FACTOR}, ${feature.location.longitude / COORD_FACTOR}`)
 	}
@@ -38,8 +38,130 @@ async function runGetFeature() {
 	])
 }
 
+async function runListFeatures() {
+	const rectangle = {
+		lo: {
+			latitude: 400000000,
+			longitude: -750000000,
+		},
+		hi: {
+			latitude: 420000000,
+			longitude: -730000000,
+		},
+	}
+	console.log('Looking for features between 40, -75 and 42, -73')
+	const call = client.listFeatures(rectangle)
+	call.on('data', (feature) => {
+		console.log(`Found feature called "${feature.name}" at ${
+			feature.location.latitude / COORD_FACTOR}, ${
+			feature.location.longitude / COORD_FACTOR}`)
+	})
+	return new Promise((resolve) => {
+		call.on('end', () => {
+			resolve()
+		})
+	})
+}
+
+function setTimeoutAsync(ms) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve()
+		}, ms)
+	})
+}
+
+function randomInt(a = 1, b = 0) {
+	const lower = Math.ceil(Math.min(a, b))
+	const upper = Math.floor(Math.max(a, b))
+	return Math.floor(lower + Math.random() * (upper - lower + 1))
+}
+
+async function runRecordRoute() {
+	let call
+	const p = new Promise((resolve, reject) => {
+		call = client.recordRoute((error, stats) => {
+			if (error) {
+				reject(error)
+				return
+			}
+			console.log('Finished trip with', stats.point_count, 'points')
+			console.log('Passed', stats.feature_count, 'features')
+			console.log('Travelled', stats.distance, 'meters')
+			console.log('It took', stats.elapsed_time, 'seconds')
+			resolve()
+		})
+	})
+
+	for (let i = 0; i < 10; i += 1) {
+		const { location: { latitude, longitude } } = feature_list[randomInt(feature_list.length - 1)]
+		console.log(new Date(), `Visiting point ${latitude / COORD_FACTOR}, ${longitude / COORD_FACTOR}`)
+		call.write({ latitude, longitude })
+		// eslint-disable-next-line no-await-in-loop
+		await setTimeoutAsync(randomInt(500, 1500))
+	}
+	call.end()
+
+	return p
+}
+
+async function runRouteChat() {
+	const call = client.routeChat()
+
+	const p = new Promise((resolve) => {
+		call.on('data', (note) => {
+			console.log(`Got message "${note.message}" at ${note.location.latitude}, ${note.location.longitude}`)
+		})
+		call.on('end', () => {
+			resolve()
+		})
+	})
+
+	const notes = [{
+		location: {
+			latitude: 0,
+			longitude: 0,
+		},
+		message: 'First message',
+	}, {
+		location: {
+			latitude: 0,
+			longitude: 1,
+		},
+		message: 'Second message',
+	}, {
+		location: {
+			latitude: 1,
+			longitude: 0,
+		},
+		message: 'Third message',
+	}, {
+		location: {
+			latitude: 0,
+			longitude: 0,
+		},
+		message: 'Fourth message',
+	}]
+	for (let i = 0; i < notes.length; i += 1) {
+		const note = notes[i]
+		console.log(`Sending message "${note.message}" at ${note.location.latitude}, ${note.location.longitude}`)
+		call.write(note)
+	}
+
+	call.end()
+
+	return p
+}
+
 async function main() {
 	await runGetFeature()
+	console.log('-----------------------------------------------')
+	await runListFeatures()
+	console.log('-----------------------------------------------')
+	await runRecordRoute()
+	console.log('-----------------------------------------------')
+	await runRouteChat()
+	console.log('-----------------------------------------------')
 }
 
 main()
--- projects/demo02/server/index.js
+++ projects/demo02/server/index.js
@@ -1,10 +1,10 @@
 const path = require('path')
-const fs = require('fs')
 // eslint-disable-next-line import/no-extraneous-dependencies
 const grpc = require('@grpc/grpc-js')
 // eslint-disable-next-line import/no-extraneous-dependencies
 const protoLoader = require('@grpc/proto-loader')
 const { port } = require('../common/config')
+const feature_list = require('../common/route_guide_db')
 
 const packageDefinition = protoLoader.loadSync(path.join(__dirname, '../common/route_guide.proto'), {
 	keepCase: true,
@@ -15,36 +15,122 @@ const packageDefinition = protoLoader.loadSync(path.join(__dirname, '../common/r
 })
 const { routeguide } = grpc.loadPackageDefinition(packageDefinition)
 
-const feature_list = JSON.parse(fs.readFileSync(path.join(__dirname, './route_guide_db.json')))
-
 function checkFeature(point) {
-	let feature
-	// Check if there is already a feature object for the given point
 	for (let i = 0; i < feature_list.length; i += 1) {
-		feature = feature_list[i]
+		const feature = feature_list[i]
 		if (feature.location.latitude === point.latitude && feature.location.longitude === point.longitude) {
 			return feature
 		}
 	}
-	const name = ''
-	feature = {
-		name,
-		location: point,
+	return {
+		name: '',
+		location: null,
 	}
-	return feature
 }
 
 function getFeature(call, callback) {
 	callback(null, checkFeature(call.request))
 }
 
+function listFeatures(call) {
+	const { lo, hi } = call.request
+	const left = Math.min(lo.longitude, hi.longitude)
+	const right = Math.max(lo.longitude, hi.longitude)
+	const top = Math.max(lo.latitude, hi.latitude)
+	const bottom = Math.min(lo.latitude, hi.latitude)
+	// For each feature, check if it is in the given bounding box
+	feature_list.forEach((feature) => {
+		if (feature.name === '') {
+			return
+		}
+		const { longitude, latitude } = feature.location
+		if (longitude >= left && longitude <= right && latitude >= bottom && latitude <= top) {
+			call.write(feature)
+		}
+	})
+	call.end()
+}
+
+const COORD_FACTOR = 1e7
+
+function getDistance(start, end) {
+	function toRadians(num) {
+		return (num * Math.PI) / 180
+	}
+	const R = 6371000 // earth radius in metres
+	const lat1 = toRadians(start.latitude / COORD_FACTOR)
+	const lat2 = toRadians(end.latitude / COORD_FACTOR)
+	const lon1 = toRadians(start.longitude / COORD_FACTOR)
+	const lon2 = toRadians(end.longitude / COORD_FACTOR)
+
+	const deltalat = lat2 - lat1
+	const deltalon = lon2 - lon1
+	const a = Math.sin(deltalat / 2) * Math.sin(deltalat / 2)
+		+ Math.cos(lat1) * Math.cos(lat2)
+		* Math.sin(deltalon / 2) * Math.sin(deltalon / 2)
+	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+	return R * c
+}
+
+function recordRoute(call, callback) {
+	let point_count = 0
+	let feature_count = 0
+	let distance = 0
+	let previous = null
+	// Start a timer
+	const start_time = process.hrtime()
+	call.on('data', (point) => {
+		point_count += 1
+		if (checkFeature(point).name !== '') {
+			feature_count += 1
+		}
+		if (previous != null) {
+			distance += getDistance(previous, point)
+		}
+		previous = point
+	})
+	call.on('end', () => {
+		callback(null, {
+			point_count,
+			feature_count,
+			// eslint-disable-next-line no-bitwise
+			distance: distance | 0,
+			elapsed_time: process.hrtime(start_time)[0],
+		})
+	})
+}
+
+const route_notes = {}
+
+function pointKey(point) {
+	return `${point.latitude} ${point.longitude}`
+}
+
+function routeChat(call) {
+	call.on('data', (note) => {
+		const key = pointKey(note.location)
+		if (route_notes[key]) {
+			route_notes[key].forEach((n) => {
+				call.write(n)
+			})
+		} else {
+			route_notes[key] = []
+		}
+		route_notes[key].push(JSON.parse(JSON.stringify(note)))
+		console.log(route_notes)
+	})
+	call.on('end', () => {
+		call.end()
+	})
+}
+
 function main() {
 	const server = new grpc.Server()
 	server.addService(routeguide.RouteGuide.service, {
 		getFeature,
-		//   listFeatures,
-		//   recordRoute,
-		//   routeChat,
+		listFeatures,
+		recordRoute,
+		routeChat,
 	})
 
 	server.bindAsync(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure(), () => {
